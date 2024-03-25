b88b3534f51268ae9d1c417b88a2b842b7c511ae
John Heid <77092876+skved@users.noreply.github.com>
2024-03-25 15:28:14 -0400

    Up to Filtering, Half implemented? But perhaps functional.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 
 	// "strings"
@@ -223,15 +224,119 @@ func main() {
 
 	// The FFT returns complex numbers, but Do I need a complex input?
 
-	complexFrames := make([][]complex128, len(frames))
-	fft := fourier.NewFFT(len(frames))
+	fftCoefficients := make([][]complex128, len(frames))
+
 	for i := 0; i < len(frames); i++ {
-		complexFrames[i] = make([]complex128, len(frames[i]))
-		for j := 0; j < len(frames[i]); j++ {
-			complexFrames[i][j] = complex(frames[i][j], 0)
-		}
-		fft.Coefficients(complexFrames[i], goingHam[i]) // The first argument should be the signal before windowing. The second argument is the windowed bit.
-		// This means I have to go back to the windowing and do it without changing the original signal.
+		fft := fourier.NewFFT(len(frames[i]))
+		fftCoefficients[i] = fft.Coefficients(nil, goingHam[i])
+		//fmt.Println("Coefficients: ", fftCoefficients[i])
+		//fmt.Println("FFT Length: ", fft.Len())
+
+		// fft.Coefficients(complexFrames[i], goingHam[i])
 	}
 
+	// https://en.wikipedia.org/wiki/Triangular_function
+	// Convolution: Operation that combines two signals to produce a third signal. - this should include/be the overlap-add step.
+
+	// https://pkg.go.dev/github.com/brettbuddin/fourier@v0.1.1#section-readme
+	// This is a possible library?
+	// also this library does FFT and they don't use make[][], just a single make[]. So I'm not sure if I'm doing it correctly.
+
+	// END FFT
+	// Start Power Spectrum - Triangular filtering Pre-Step
+
+	// I'm squaring the magnitude (so abs value) of the complex numbers, But im extracting the real numbers and discarding the imaginary numbers.
+	// Import the math library
+
+	powerSpectrum := make([][]float64, len(fftCoefficients))
+	for i := 0; i < len(fftCoefficients); i++ {
+		powerSpectrum[i] = make([]float64, len(fftCoefficients[i]))
+		for j := 0; j < len(fftCoefficients[i]); j++ {
+			magnitude := math.Sqrt(real(fftCoefficients[i][j])*real(fftCoefficients[i][j]) + imag(fftCoefficients[i][j])*imag(fftCoefficients[i][j]))
+			powerSpectrum[i][j] = magnitude * magnitude
+		}
+	}
+	//fmt.Println("Power Spectrum: ", powerSpectrum)
+
+	// End Power Spectrum
+
+	// Start Triangular Filtering - Mel Filter Banks
+	// https://www.statistics.com/glossary/triangular-filter/#:~:text=As%20compared%20to%20the%20rectangular,short%20course%20Time%20Series%20Forecasting%20
+	// https://en.wikipedia.org/wiki/Mel_scale
+	// https://en.wikipedia.org/wiki/Periodogram
+
+	// Take the magnitude squared of the complex Fourier coefficients - This is the power spectrum
+	// Map the power spectrum onto the Mel scale using a filterbank.
+
+	// https://en.wikipedia.org/wiki/Mel_scale
+	//melScale := 2595 * math.Log10(1+(sampleRate/2)/700) // Divide by 2 for Nyquist frequency
+	//filterBankSize := 26                                // Number of filters
+
+	// NOTE! This is NOT like windowing/framing. Framinng we can think of as something we applied horizontally all along the data. The filter(s) we are applying we apply vertically.
+
+	// So first apply the melscale to the power spectrum?
+	// then find values for the filter banks?
+	// then apply the filter banks to the power spectrum?
+
+	// Map the power spectrum onto the Mel scale
+	// melScale := 2595 * math.Log10(1+(each_index_of_powerSpectrum/2)/700) // Divide by 2 for Nyquist frequency
+
+	// May want to make a new slice to have a new name for clarity
+	for i := 0; i < len(powerSpectrum); i++ {
+		for j := 0; j < len(powerSpectrum[i]); j++ {
+			powerSpectrum[i][j] = 2595 * math.Log10(1+(float64(powerSpectrum[i][j])/2)/700) // Divide by 2 for Nyquist frequency
+		}
+	}
+
+	// ---Filter Bank Process---
+	// The filter banks are vertical?
+	// Choose relevant filter bank values based on...?
+	// Apply the filter banks to the power spectrum
+	// So for the filtering, we calculate the weights based on adjacent indices?
+
+	// For range of PowerSpectrum
+	// multiply x adjacent indices by the filter bank values
+	// add them together to get the weighted sum?
+	// take the log of the result
+
+	// Does every filterbank get applied across the entire spectrum? or is it dynamic, one filter per "section size"?
+	// Meaning the entire set of filterbanks applied to every part of the spectrum.
+
+	// End Triangular Filtering
+	// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] - indexes
+	// [1, 1, 1, 1, 1, 1, 1, 1, 1, 1] - values
+
+	// Weighted output through summing
+	// [0+12, 0+1+23, 01+2+34, 12+3+45, 23+4+56, 34+5+67, 45+6+78, 56+7+89, 67+8+9, 78+9]
+
+	// [0.000 0.053 0.105 0.158 0.211 0.263 0.316 0.368 0.421 0.474 0.526 0.579 0.632 0.684 0.737 0.789 0.842 0.895 0.947 1.000, 0.947,]
+
+	// Add 20 zeros to the beginning and end of the power spectrum as padding for the kernel
+	paddedSpectrum := make([][]float64, len(powerSpectrum))
+	for i := 0; i < len(powerSpectrum); i++ {
+		temp := make([]float64, len(powerSpectrum[i])+40)
+		copy(temp[20:], powerSpectrum[i])
+		paddedSpectrum[i] = temp
+	}
+
+	// My understanding is that for the kernel, if it is a kernel of width 40, the actual length is 41 so that the center is 1.0?
+	triangularkernel := []float64{0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0, 0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.50, 0.45, 0.40, 0.35, 0.30, 0.25, 0.20, 0.15, 0.10, 0.05}
+
+	fmt.Println("I made it to here")
+	weightedOutput := make([][]float64, len(powerSpectrum))
+	for i := 0; i < len(powerSpectrum); i++ { // Index of slices
+		weightedOutput[i] = make([]float64, len(powerSpectrum[i]))
+		for j := 0; j < len(powerSpectrum[i]); j++ { // Index of values
+			for k := 0; k < len(triangularkernel); k++ { // Index of kernel
+				weightedOutput[i][j] += paddedSpectrum[i][j+k] * triangularkernel[k]
+			}
+		}
+	}
+	fmt.Println(weightedOutput[0])
+
 }
+
+// Important notes to revisit
+// Dynamic vs Static Framing
+// Discard half of the FFT? Only compute the first half of the FFT? Only the first half contains unique information?
+// filterbank/more filters? Frequency bins is huh?
